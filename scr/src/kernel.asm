56029f30b5bf11bc503d391333fe1843de3d6660
NHTHEBEST <noreply.nhthebest@gmail.com>
2018-04-30 10:34:21 +1200

    mabey

--- scr/src/kernel.asm
+++ scr/src/kernel.asm
@@ -7,7 +7,19 @@ section         .text
         
 global start
 extern kmain            ; this function is gonna be located in our c code(kernel.c)
-start:
+app_call_api:
+        jmp os_start
+        jmp print_string
+        jmp print_ch
+os_start:
         cli             ;clears the interrupts 
         call kmain      ;send processor to continue execution from the kamin funtion in c code
         hlt             ; halt the cpu(pause it from executing from this address
+
+print_string:
+        extern print
+        call print
+
+print_ch:
+        extern printch
+        call printch
