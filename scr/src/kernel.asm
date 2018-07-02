55b429adb047f7eeeb379f7bcff9ef69c4d1940f
NHTHEBEST <noreply.nhthebest@gmail.com>
2018-07-02 16:14:59 +1200

    cf

--- scr/src/kernel.asm
+++ scr/src/kernel.asm
@@ -1,3 +1,47 @@
+
+;   call graphics_mode  ; Uncomment if you want to switch to graphics mode 0x13
+    lgdt [gdtr]
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
+    jmp sss
+
+graphics_mode:
+    mov ax, 0013h
+    int 10h
+    ret
+
+
+gdt_start:
+    dd 0                ; null descriptor--just fill 8 bytes
+    dd 0
+
+gdt_code:
+    dw 0FFFFh           ; limit low
+    dw 0                ; base low
+    db 0                ; base middle
+    db 10011010b        ; access
+    db 11001111b        ; granularity
+    db 0                ; base high
+
+gdt_data:
+    dw 0FFFFh           ; limit low (Same as code)
+    dw 0                ; base low
+    db 0                ; base middle
+    db 10010010b        ; access
+    db 11001111b        ; granularity
+    db 0                ; base high
+end_of_gdt:
+
+gdtr:
+    dw end_of_gdt - gdt_start - 1   ; limit (Size of GDT)
+    dd gdt_start        ; base of GDT
+
+    CODE_SEG equ gdt_code - gdt_start
+    DATA_SEG equ gdt_data - gdt_start
+
+
+sss:
 bits    32
 section         .text
         align   4
@@ -24,3 +68,4 @@ entering_v86:
    push dword [ebp+12]        ; cs
    push dword  [ebp+16]       ; eip
    iret
+
