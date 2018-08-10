dbd2a493470f5afe2ed644f7f99b5cf802c2c484
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2018-08-10 12:01:46 +1200

    make

--- make.py
+++ make.py
@@ -1,19 +1,40 @@
-#!/bin
+#!/usr/bin/env python
 
 import glob
+import os
+
+gcc = ""
+nasm = "" 
+ld = ""
+ldfile = ""
+objcopy = ""
 
 def gcc(filepath,out):
+    return os.system(gcc+" -m32 -c -ffreestanding +filepath+" -o +out)
+
 
 def nasm(filepath,out):
+    return os.system(nasm+" -f elf32 "+filepath+" -o "+out)
 
-def ld(filepath,out):
+
+def ld(filepaths,out,type = 1):
+    if type:
+        return os.system(ld+" -r -m elf_i386 -T "+ldfile+" -o "+out+" "+filepaths)
+    else:
+        return os.system(ld+" -m elf_i386 -T "+ldfile+" -o "+out+" "+filepaths)
+
 
 def get_dirs(dirpath):
     return glob.glob(dirpath+"/*/")
 
+
 def get_c_files(dirpath):
     return glob.glob(dirpath+"/*.c")
+
 
 def get_asm_files(dirpath):
     return glob.glob(dirpath+"/*.asm")
 
+
+def get_object_files(dirpath):
+    return glob.glob(dirpath+"/*.o")
