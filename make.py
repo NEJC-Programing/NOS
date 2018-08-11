31e41d2ae782d50ded4b72a45b94838bd294a0a3
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2018-08-11 14:50:53 +1200

    work on make.py

--- make.py
+++ make.py
@@ -3,26 +3,27 @@
 import glob
 import os
 
-gcc = ""
-nasm = "" 
-ld = ""
-ldfile = ""
+root = "/mnt/c/Users/noam/Desktop/NOS/scr/"
+compiler = "gcc"
+asembler = "nasm" 
+linker = "ld"
+ldfile = root+"src/link.ld"
 objcopy = ""
 strip = ""
 
 def gcc(filepath,out):
-    return os.system(gcc+" -m32 -c -ffreestanding +filepath+" -o +out)
+    return os.system(compiler+" -m32 -c -ffreestanding " +filepath+" -o "+out)
 
 
 def nasm(filepath,out):
-    return os.system(nasm+" -f elf32 "+filepath+" -o "+out)
+    return os.system(asembler+" -f elf32 "+filepath+" -o "+out)
 
 
-def ld(filepaths,out,type = 1):
-    if type:
-        return os.system(ld+" -r -m elf_i386 -T "+ldfile+" -o "+out+" "+filepaths)
+def ld(filepaths,out,types = True):
+    if types:
+        return os.system(linker+" -r -m elf_i386 -T "+ldfile+" -o "+out+" "+filepaths)
     else:
-        return os.system(ld+" -m elf_i386 -T "+ldfile+" -o "+out+" "+filepaths)
+        return os.system(linker+" -m elf_i386 -T "+ldfile+" -o "+out+" "+filepaths)
 
 
 def get_dirs(dirpath):
@@ -40,6 +41,26 @@ def get_asm_files(dirpath):
 def get_object_files(dirpath):
     return glob.glob(dirpath+"/*.o")
 
+
+
+def run(dir_to_run, notroot = True):
+    dirs = get_dirs(dir_to_run)
+    for x in dirs:
+        run(x)
+    cs = get_c_files(dir_to_run)
+    for x in cs:
+        gcc('"'+x+'"', '"'+x+'.o"')
+    asms = get_asm_files(dir_to_run)
+    for x in asms:
+        nasm('"'+x+'"', '"'+x+'.o"')
+    objects = get_object_files(dir_to_run)
+    o = ""
+    for x in objects:
+        o = o+" \""+x+'"'
+    ld(o,dir_to_run+"../",notroot)
+
+
+run(root,False)
 
 """
 make dir tree
