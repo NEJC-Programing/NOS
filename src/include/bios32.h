d3b99094e16cb05fee31ea1caae5b0d21cc078c3
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2020-05-21 10:31:13 +1200

    Update bios32.h

--- src/include/bios32.h
+++ src/include/bios32.h
@@ -4,5 +4,5 @@ typedef struct __attribute__ ((packed)) {
 	unsigned short di, si, bp, sp, bx, dx, cx, ax;
 	unsigned short gs, fs, es, ds, eflags;
 } regs16_t;
-extern void biosint32(unsigned char intnum, regs16_t *regs);
+extern void _cdelc biosint32(unsigned char intnum, regs16_t *regs);
 #endif
